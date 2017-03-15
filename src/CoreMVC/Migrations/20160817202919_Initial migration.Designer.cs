90b8afbb46bfa23f765bad349bf31a7b6b90e678
Marouane-A <marouane.allagui@esprit.tn>
2017-03-15 18:55:41 +0100

    Creating first version of showroomer core

---
+++ src/CoreMVC/Migrations/20160817202919_Initial migration.Designer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MVC.Models;
+
+namespace MVC.Migrations
+{
+    [DbContext(typeof(ElephantDbContext))]
+    [Migration("20160817202919_Initial migration")]
+    partial class Initialmigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("Npgsql:PostgresExtension:.uuid-ossp", "'uuid-ossp', '', ''")
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431");
+
+            modelBuilder.Entity("EFCoreToDo.ToDoItem", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Text");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("ToDoItems");
+                });
+        }
+    }
+}
